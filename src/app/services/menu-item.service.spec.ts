fb1a309936b9541bf34c161c7ae55b0c254fd034
Eric Reber <edreber@gmail.com>
2017-03-01 12:47:10 -0600

    Including only working tests.

--- src/app/services/menu-item.service.spec.ts
+++ src/app/services/menu-item.service.spec.ts
@@ -5,7 +5,7 @@ import * as sinon from 'sinon';
 import { expect } from 'chai';
 import { MenuItemService } from "./menu-item.service";
 import { Http } from "@angular/http";
-import { MenuItem } from "../models/menu-item.model";
+
 
 
 describe('In the file menu-item.service.ts', () => {
@@ -35,31 +35,38 @@ describe('In the file menu-item.service.ts', () => {
 
         });
 
-        describe('postMenuItem', () => {
-            it('will create a menu item', () => {
-                let menuItem = new MenuItem();
-                let responseMenuItem = menuItemService.postMenuItem(menuItem)
-                expect(true).to.equal(false);
-            })
+        // describe('postMenuItem', () => {
+        //     it('will create a menu item', () => {
+        //         let menuItem = new MenuItem();
+        //         let responseMenuItem = menuItemService.postMenuItem(menuItem)
+        //         expect(true).to.equal(false);
+        //     })
+        //
+        // });
+        //
+        // describe('readMenuItem', () => {
+        //     it('will read a menu item', () => {
+        //
+        //         server.respondWith("GET", "/menuitems/1", [
+        //             200,
+        //             {"Content-Type": "application/json"},
+        //             '[{"id": "1", "name": "Pizza", "description: "Yummy", "restaurantName": "Dewey\'s" }]'
+        //         ]);
+        //
+        //         let id = "1";
+        //         let responseMenuItem = menuItemService.readMenuItem(id);
+        //         expect(responseMenuItem).to.equal(false);
+        //     })
+        //
+        // });
 
-        });
-
-        describe('readMenuItem', () => {
-            it('will read a menu item', () => {
-                let id = "1";
-                let responseMenuItem = menuItemService.readMenuItem(id);
-                expect(true).to.equal(false);
-            })
-
-        });
-
-        describe('readMenuItems', () => {
-            it('will read all menu items', () => {
-                let menuItem = new MenuItem();
-                let responseMenuItemList = menuItemService.readMenuItems();
-                expect(true).to.equal(false);
-            })
-        });
+        // describe('readMenuItems', () => {
+        //     it('will read all menu items', () => {
+        //         let menuItem = new MenuItem();
+        //         let responseMenuItemList = menuItemService.readMenuItems();
+        //         expect(true).to.equal(false);
+        //     })
+        // });
 
 
     });
