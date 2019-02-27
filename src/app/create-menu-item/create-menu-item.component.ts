5ee2cdccffbb7de643a875331f6aff93f91fdc95
Pradeep K P <pk00525512@techmahindra.com>
2019-02-27 16:31:46 +0530

    Change to FTA Exception Handling

--- src/app/create-menu-item/create-menu-item.component.ts
+++ src/app/create-menu-item/create-menu-item.component.ts
@@ -18,15 +18,42 @@ export class CreateMenuItemComponent {
         this.features = features;
     }
 
+    name: string;
+    message: string;
+
+    MyException(message) {
+        this.name = "NullValuesException"
+        this.message = message;
+    }
+
+    LargeDescription(message) {
+        this.name = "LargeDescriptionException"
+        this.message = message;
+    }
+
     submitForm(form: NgForm) {
-        this.menuItemService.postMenuItem(this.menuItem)
+        if(this.menuItem.name == null && this.menuItem.restaurantName == null &&
+            this.menuItem.description == null) { 
+                this.MyException.prototype =new Error();
+                this.MyException.prototype.constructor=this.MyException;
+                throw new this.MyException('Fields are empty!');
+            }
+        else if (this.menuItem.description.length>20)
+        {
+            this.LargeDescription.prototype =new Error();
+            this.LargeDescription.prototype.constructor=this.LargeDescription;
+            throw new this.LargeDescription('Description cannot be more than 20 characters');
+        }
+        else
+            {
+            this.menuItemService.postMenuItem(this.menuItem)
             .subscribe(
                 data =>{console.log('Success. Received response:  ', data),
                 this.router.navigate(['/view-list'])},
                 err => {console.log('error: ', err),
-                this.router.navigate(['/view-list'])}
-            )
-        
+                this.router.navigate(['/view-list'])}               
+            )              
+            }
     }
 }
 
